a5a478f6cd4e6f52f454e16af78fc5fd2256d02c
MarioSantoro <mariosantoro124@gmail.com>
2023-05-25 14:01:02 +0200

    Create a timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const Lesson = new Date("May 26, 2023 9:30").getTime();
 console.log(Lesson);
 
-
+let finishCounter =setInterval(CountDown , 1000);
 
 function CountDown(){
     let DateNow = new Date().getTime();
@@ -13,4 +13,6 @@ function CountDown(){
     let minutes = Math.floor((distanceOflesson % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distanceOflesson % (1000 * 60)) / 1000);
 
+    const time = document.getElementById("time");
+    time.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 }
